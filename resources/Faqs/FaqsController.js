0587ff3d3c73e6a7815a70c6a9a379b878b6cafe
Pratish Ninawe <pratish.1006@gmail.com>
2023-10-19 13:41:11 +0530

    feat - added edit and delete faq apis

--- resources/Faqs/FaqsController.js
+++ resources/Faqs/FaqsController.js
@@ -49,3 +49,87 @@ export const getAllFaqs = async (req, res) => {
     });
   }
 };
+
+export const getFaqById = async (req, res) => {
+  try {
+    if (!req?.user) {
+      return res.status(400).json({ message: "Please login!" });
+    }
+
+    const faqId = req.params.id;
+
+    const faq = await Faqs.findOne({ _id: faqId, added_by: req.user._id });
+
+    if (!faq) {
+      return res.status(404).json({
+        message: "FAQ not found or you don't have permission to access it",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      faq,
+      message: "FAQ retrieved successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message ? error.message : "Something went wrong",
+    });
+  }
+};
+
+export const updateFaq = async (req, res) => {
+  try {
+    if (!req?.user) return res.status(400).json({ message: "please login !" });
+    const faqId = req.params.id;
+
+    const faq = await Faqs.findOne({ _id: faqId, added_by: req.user._id });
+
+    if (!faq) {
+      return res
+        .status(404)
+        .json({ message: "FAQ not found or you don't have permission" });
+    }
+
+    faq.question = req.body.question || faq.question;
+    faq.answer = req.body.answer || faq.answer;
+
+    await faq.save();
+
+    res.status(200).json({
+      success: true,
+      faq,
+      message: "FAQ updated successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message ? error.message : "Something went Wrong",
+    });
+  }
+};
+
+export const deleteFaq = async (req, res) => {
+  try {
+    if (!req?.user) return res.status(400).json({ message: "please login !" });
+    const faqId = req.params.id;
+
+    const faq = await Faqs.findOne({ _id: faqId, userId: req.user.id });
+
+    if (!faq) {
+      return res
+        .status(404)
+        .json({ message: "FAQ not found or you don't have permission" });
+    }
+
+    await Faqs.findByIdAndRemove(faqId);
+
+    res.status(200).json({ message: "FAQ deleted successfully" });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message ? error.message : "Something went Wrong",
+    });
+  }
+};
--- resources/Faqs/FaqsRoute.js
+++ resources/Faqs/FaqsRoute.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { isAuthenticatedUser, authorizeRoles } from "../../middlewares/auth.js";
-import { AddNewFaqs, getAllFaqs } from "./FaqsController.js";
+import {
+  AddNewFaqs,
+  deleteFaq,
+  getAllFaqs,
+  getFaqById,
+  updateFaq,
+} from "./FaqsController.js";
 
 const router = express.Router();
 
@@ -10,5 +16,14 @@ router
 router
   .route("/getAll")
   .get(isAuthenticatedUser, authorizeRoles("admin"), getAllFaqs);
+router
+  .route("/get/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getFaqById);
+router
+  .route("/update/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateFaq);
+router
+  .route("/delete/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteFaq);
 
 export default router;
